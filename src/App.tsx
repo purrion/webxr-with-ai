a945780a108040f73562ca56d1fc05f0c7c55c79
Le Viet Dat <lvdat.roy@gmail.com>
2025-10-17 20:23:21 +0700

    feat: check AR supported

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,68 @@
 import { Canvas } from "@react-three/fiber";
-import { XR, createXRStore } from "@react-three/xr";
+import {
+  createXRStore,
+  IfInSessionMode,
+  useXRSessionModeSupported,
+  XR,
+} from "@react-three/xr";
 import { useState } from "react";
 import "./App.css";
 
+const sessionMode: XRSessionMode = "immersive-ar";
 const store = createXRStore();
 
 export default function App() {
-  const [red, setRed] = useState(false);
+  const [isARSupported, setIsARSupported] = useState<boolean | null>(null);
+  const browserSupported = useXRSessionModeSupported(sessionMode);
+
+  async function handleCheckARManually() {
+    if (!browserSupported || !navigator.xr) {
+      setIsARSupported(false);
+      return;
+    }
+    try {
+      const session = await navigator.xr.requestSession(sessionMode, {
+        requiredFeatures: ["local-floor"],
+        optionalFeatures: ["hit-test"],
+      });
+      await session.end();
+      setIsARSupported(true);
+    } catch (err) {
+      console.error("❌ AR failed:", err);
+      setIsARSupported(false);
+    }
+  }
+
   return (
-    <>
-      <button onClick={() => store.enterAR()}>Enter AR</button>
-      <Canvas>
-        <XR store={store}>
-          <mesh
-            pointerEventsType={{ deny: "grab" }}
-            onClick={() => setRed(!red)}
-            position={[0, 1, -1]}
-          >
-            <boxGeometry />
-            <meshBasicMaterial color={red ? "red" : "blue"} />
-          </mesh>
-        </XR>
-      </Canvas>
-    </>
+    <div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <button type="button" onClick={handleCheckARManually}>
+          Check AR Support
+        </button>
+
+        <button
+          type="button"
+          onClick={() => store.enterAR()}
+          disabled={!isARSupported}
+        >
+          Open app
+        </button>
+      </div>
+
+      <p>
+        {isARSupported != null &&
+          `AR is ${isARSupported ? "" : "not"} supported on this device`}
+      </p>
+
+      {isARSupported && (
+        <Canvas>
+          <XR store={store}>
+            <IfInSessionMode deny={sessionMode}>
+              {/*<color args={["red"]} attach="background" />*/}
+            </IfInSessionMode>
+          </XR>
+        </Canvas>
+      )}
+    </div>
   );
 }
